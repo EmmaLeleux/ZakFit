cf46352224cffc503d81b19fc5c0a187fd30c24a
Emma Leleux <leleux.emma@gmail.com>
2025-11-27 10:24:35 +0100

    création du projet

---
+++ ZakFit/ZakFit/ZakFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZakFitApp.swift
+//  ZakFit
+//
+//  Created by Emma on 27/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ZakFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
